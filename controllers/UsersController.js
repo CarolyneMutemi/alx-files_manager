dd3a45439f3cb72e0f9fd1df288670ce795c0f32
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-05-12 20:18:44 +0300

    Reverting to the initial set up.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,11 @@
-import Users from '../utils/users';
+import { createHash } from 'crypto';
+import dbClient from '../utils/db';
 
 export default class UsersController {
   static async postNew(req, res) {
     const { email } = req.body;
     const { password } = req.body;
+    const users = dbClient.db.collection('users');
 
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
@@ -13,14 +15,18 @@ export default class UsersController {
       return res.status(400).json({ error: 'Missing password' });
     }
 
-    let user = await Users.getUser(email)
-
-    if (user) {
+    if (await users.findOne({ email })) {
       return res.status(400).json({ error: 'Already exist' });
     }
 
-    await Users.addUser(email, password)
-    user = await Users.getUser(email)
+    const hash = createHash('sha1');
+    hash.update(password);
+    const hashedPassword = hash.digest('hex');
+
+    const doc = { email, password: hashedPassword };
+    users.insertOne(doc);
+
+    const user = await users.findOne({ email }, { projection: { _id: 1, email: 1 } });
 
     return res.status(201).json(user);
   }
