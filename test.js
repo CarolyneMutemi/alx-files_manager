b8ec3890cd80ada893af4bd500c03040fa0cba42
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-05-14 01:48:12 +0300

    Linting error.

--- test.js
+++ test.js
@@ -1,18 +1,16 @@
-import redisClient from "./utils/redis"
+import redisClient from './utils/redis';
 
-//const auth = Buffer.from("Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=", 'base64').toString()
-//const pattern = /:(.+)/
-//const splitAuthData = "bob@dylan.com:toto:12:34!".split(pattern)
-//const userName = splitAuthData[0]
-//const password = splitAuthData[1]
+const auth = Buffer.from('Ym9iQGR5bGFuLmNvbTp0b3RvMTIzNCE=', 'base64').toString();
+const pattern = /:(.+)/;
+const splitAuthData = auth.split(pattern);
+const userName = splitAuthData[0];
+const password = splitAuthData[1];
 
-//console.log(userName, password)
+console.log(userName, password);
 
-(async () => {
-redisClient.del('auth_8b83aed4-48bc-4b15-ab51-cfd843c3494b')})();
+// (async () => {
+// redisClient.del('auth_8b83aed4-48bc-4b15-ab51-cfd843c3494b')})();
 
+// const hey = undefined
 
-
-const hey = undefined
-
-console.log(hey == undefined)
+// console.log(hey == undefined)
--- utils/users.js
+++ utils/users.js
@@ -1,20 +1,20 @@
-import dbClient from "./db";
-import {createHash} from "crypto";
+import { createHash } from 'crypto';
+import dbClient from './db';
 
 export default class Users {
-    static usersCollection = dbClient.db.collection('users');
+  static async addUser(email, password) {
+    const usersCollection = dbClient.db.collection('users');
+    const hash = createHash('sha1');
+    hash.update(password);
+    const hashedPassword = hash.digest('hex');
 
-    static async addUser(email, password){
-        const hash = createHash('sha1');
-        hash.update(password);
-        const hashedPassword = hash.digest('hex');
+    const doc = { email, password: hashedPassword };
+    await usersCollection.insertOne(doc);
+  }
 
-        const doc = { email, password: hashedPassword };
-        await Users.usersCollection.insertOne(doc);
-    }
-
-    static async getUser(email){
-        const user = await Users.usersCollection.findOne({ email }, { projection: { _id: 1, email: 1 } });
-        return user
-    }
+  static async getUser(email) {
+    const usersCollection = dbClient.db.collection('users');
+    const user = await usersCollection.findOne({ email }, { projection: { _id: 1, email: 1 } });
+    return user;
+  }
 }
